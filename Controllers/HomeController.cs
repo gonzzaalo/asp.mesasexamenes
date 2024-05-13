6ece9f82e7e9bb7779f65d5cc8add0dd6985bec2
gonzzaalo <gonzalxc@gmail.com>
2024-05-13 16:45:56 -0300

    Inicio del Proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Inscripciones.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Inscripciones.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
+        public IActionResult Aniversario()
+        {
+            return View();
+        }
+
+        public IActionResult Ajedrez()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
